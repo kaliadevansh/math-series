692489f9e7ffd5817de5c989c8f58f4474bc1c18
kaliadevansh <kaliadevansh@gmail.com>
2020-06-13 18:41:57 +0530

    Code lucas requirement

--- math_series/series.py
+++ math_series/series.py
@@ -16,3 +16,18 @@ def fibonacci(n):
     elif n > 1:
         return fibonacci(n - 1) + fibonacci(n - 2)
     return n
+
+
+def lucas(n):
+    """
+    This function returns the nth lucas number. The function is built using recursion.
+    :param n: Index of number of lucas series to be returned. The series starts at 0.
+    :return: n-th number from the lucas series.
+    """
+    if not isinstance(n, int):
+        raise TypeError()
+    elif n < 0:
+        raise IndexError()
+    elif n > 1:
+        return lucas(n - 1) + lucas(n - 2)
+    return 2 - n
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,6 +1,6 @@
 import pytest
 
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 
 # fibonacci tests
@@ -52,3 +52,55 @@ def test_fib_none():
 def test_fib_negative():
     with pytest.raises(IndexError):
         fibonacci(-1)
+
+
+# lucas tests
+
+def test_lucas_0():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+
+def test_lucas_1():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_lucas_2():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+
+def test_lucas_3():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+
+def test_lucas_7():
+    actual = lucas(7)
+    expected = 29
+    assert actual == expected
+
+
+def test_lucas_float():
+    with pytest.raises(TypeError):
+        lucas(7.7)
+
+
+def test_lucas_string():
+    with pytest.raises(TypeError):
+        lucas("7")
+
+
+def test_lucas_none():
+    with pytest.raises(TypeError):
+        lucas(None)
+
+
+def test_lucas_negative():
+    with pytest.raises(IndexError):
+        lucas(-1)
